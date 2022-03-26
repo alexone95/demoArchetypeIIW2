fcc7b33af07bf802da113d0a48b96fa57e8eae4a
alessandro <alessandropontis95@gmail.com>
2022-03-26 18:15:46 +0100

    first commit

---
+++ src/main/java/DemoClass.java
@@ -0,0 +1,16 @@
+public class DemoClass {
+
+    private String ciao;
+
+    public String getCiao() {
+        return ciao;
+    }
+
+    public void setCiao(String ciao) {
+        this.ciao = ciao;
+    }
+
+    public DemoClass(String ciao) {
+        this.ciao = ciao;
+    }
+}
